2ad909b03a11bc2b407407a5e53e4ed8da48e31c
rbattezzati <rafael.battezzati@gmail.com>
2025-11-11 11:00:32 +0100

    Final version

--- services/gateway/src/main/java/com/rbattezzati/gateway/security/SecurityConfig.java
+++ services/gateway/src/main/java/com/rbattezzati/gateway/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.rbattezzati.gateway.security;
 
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
